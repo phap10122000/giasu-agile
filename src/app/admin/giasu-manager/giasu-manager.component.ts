13dddc9237d786ffb2cb9d9a5a92b7c613fbc94a
phap10122000 <trinhhaphap@gmail.com>
2020-07-30 09:26:22 +0700

    1 file modified

--- src/app/admin/giasu-manager/giasu-manager.component.ts
+++ src/app/admin/giasu-manager/giasu-manager.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from '../Product';
+import { ProductService } from '../product.service';
 @Component({
   selector: 'app-giasu-manager',
   templateUrl: './giasu-manager.component.html',
   styleUrls: ['./giasu-manager.component.css']
 })
 export class GiasuManagerComponent implements OnInit {
-
-  constructor() { }
+   products: Product[];
+  selected: Product;
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.getProducts();
   }
-
+  getProducts() {
+    this.productService.listProducts().subscribe(data =>{
+      this.products = data;
+    })
+    // this.products = this.productService.listProducts();
+  }
 }
