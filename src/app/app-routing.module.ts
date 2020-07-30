31c2e5be24a0728a75ff235badfbb13cc80bc71e
phap10122000 <trinhhaphap@gmail.com>
2020-07-30 09:28:20 +0700

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { AdminComponent } from './admin/admin.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { ContactComponent } from './contact/contact.component';
-import { NewsComponent } from './news/news.component';
 import { ServiceComponent } from './service/service.component';
+import { NewsComponent } from './news/news.component';
 
 
 const routes: Routes = [
@@ -16,14 +16,15 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'about', component: AboutComponent},
   { path: 'contact', component: ContactComponent},
-  { path: 'login', component: LoginComponent},
+  { path: 'service', component: ServiceComponent},
   { path: 'news', component: NewsComponent},
+  { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignUpComponent},
   { path: 'admin', component: AdminComponent,
     children: [
         { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
         { path: 'dashboard', component: DashboardComponent},
-        { path: 'manager', component: DashboardComponent}
+        { path: 'managar', component: DashboardComponent}
     ]
   } 
 ];
